d0de21ccc519053bb41d9db2411d50ba629a9f9c
Marcus <pths411027@gmail.com>
2024-06-27 22:42:04 +0800

    feat: init commet

---
+++ app/config.ts
@@ -0,0 +1,28 @@
+export const defaultText = "Switch Card";
+
+export const frameworks = [
+  {
+    img: "React.jpeg",
+    title: "React",
+    des: "React is a JavaScript library for building user interfaces. Its core principles are declarative code and efficiency. Its main goal is to be fast, simple, and scalable.",
+    url: "https://zh-hant.legacy.reactjs.org/",
+  },
+  {
+    img: "Vue.jpeg",
+    title: "Vue",
+    des: "Vue is a progressive JavaScript framework for building user interfaces. It is designed from the ground up to be incrementally adoptable. Its core library is focused on the view layer only.",
+    url: "https://vuejs.org/",
+  },
+  {
+    img: "Angular.jpeg",
+    title: "Angular",
+    des: "Angular is a platform and framework for building single-page client applications using HTML and TypeScript. Its open-source framework is maintained by Google and individuals.",
+    url: "https://angular.tw/",
+  },
+  {
+    img: "Svelte.jpeg",
+    title: "Svelte",
+    des: "Svelte is a radical new approach to building user interfaces. It shifts the work of building applications from the client-side to the compile step. Its goal is to write less .",
+    url: "https://svelte.dev/",
+  },
+];
